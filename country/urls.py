eb88e42a9b85eb758fe52de3b511c721445a7938
Rayhan <pialshek08@gmail.com>
2025-02-19 02:32:36 +0600

    List neighbor contries of a specific conuntyr done

--- country/urls.py
+++ country/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CountryViewSet
+from .views import CountryViewSet,NeighbourCountryView
 
 # Create a router and register the viewset
 router = DefaultRouter()
@@ -8,4 +8,6 @@ router.register(r'countries', CountryViewSet)
 
 urlpatterns = [
     path('country/', include(router.urls)),
+    path('neighbor/<str:country>', NeighbourCountryView.as_view()),
+
 ]
--- country/views.py
+++ country/views.py
@@ -3,6 +3,10 @@ from . models import Country
 from . serializers import CountrySerializer
 # Create your views here.
 from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+
 
 from django_filters import rest_framework as filters
 
@@ -12,6 +16,12 @@ class CountryViewSet(viewsets.ModelViewSet):
     # filter_backends = (filters.DjangoFilterBackend,)
     # filterset_fields = ('name', 'region')
 
+class NeighbourCountryView(APIView):
+    def get(self, request, country):
+        country = get_object_or_404(Country, name=country)
+        neighbors = Country.objects.filter(region=country.region).exclude(name=country)
+        serializer = CountrySerializer(neighbors, many=True)
+        return Response(serializer.data)
 
 
 
